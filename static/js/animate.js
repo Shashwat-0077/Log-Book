4d50e26cc1d27fca018977da2b4939bb336330fa
Shashwat <shashwatg0911@gmail.com>
2022-09-27 01:40:28 +0530

    added suggestion list

--- static/js/animate.js
+++ static/js/animate.js
@@ -2,6 +2,11 @@ const tabs = Array.from(document.querySelectorAll(".tab"));
 
 tabs.forEach((tab) => {
     tab.addEventListener("click", (e) => {
+        if (tab.classList.contains("active")) {
+            tab.classList.remove("active");
+            return;
+        }
+
         tabs.forEach((innerTab) => {
             innerTab.classList.remove("active");
         });
